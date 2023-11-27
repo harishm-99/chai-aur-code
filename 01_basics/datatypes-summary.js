4a402e0ca0066c68fee18ba0a1579fd70ec83c54
Harish Mahato <101078273+harishm-99@users.noreply.github.com>
2023-11-27 06:48:48 +0000

    datatypes completed

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,4 +1,41 @@
 // #Primitive 
 
-// 7 types : String, Number, Boolean, 
+// 7 types : String, Number,Boolean,null,undefined,symbol, BigInt
+
+// Js is a dynamically typed language, dt need not to be defined for compilation
+
+const score = 25;// Number value
+const scoreValue = 56.2;//Decimal is also number value
+const isLoggedin = false;// Boolean  value
+const outsideTemp = null;
+let userMail;// undefined value
+
+const id = Symbol('123');
+const anotherID = Symbol('123');
+
+console.log(id === anotherID);// Assigns Unique values even if the values are identical.
+
+const bigNumber = 12236467787875454n;// bigInt Value
+
+// Reference Type (Non-Primitives)
+
+// Arrays, Objects, Functions
+
+const heroes = ["shaktimaan", "nagraj", "doga"];// Arrays Values
+
+let myObj = {
+    name: "Harish",
+    age: 24
+}//Object Values
+
+const myFunction = function () {
+    console.log("Hello World");
+};
+
+console.log(typeof (myFunction));
+console.log(typeof (bigNumber));
+console.log(typeof (outsideTemp));// The data type of null shows object
+
+
+
 
