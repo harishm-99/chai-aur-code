c1ba509fc3dead287f95b2e2a775c6d201a52249
Harish Mahato <101078273+harishm-99@users.noreply.github.com>
2023-11-27 18:16:03 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 15677;
+let accountEmail = "harish@gmail.com";
+var accountPassword = "123456789"
+accountCity = "Bengaluru";
+
+// accountId = 2;
+
+accountEmail = "harish@google.com";
+accountPassword = "098765";
+accountCity = "Jaipur";
+
+console.table(accountId, accountEmail, accountPassword, accountCity);
