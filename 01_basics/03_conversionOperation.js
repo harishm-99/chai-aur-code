a619b607361d16d7dd83779742ffeb7768ab523a
Harish Mahato <101078273+harishm-99@users.noreply.github.com>
2023-11-28 10:26:36 +0000

    conversion completed

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -38,3 +38,46 @@ console.log(typeof (stringNumber));
 
 //*********************  Operations  ************************** */
 
+let value = 3;
+let negValue = -value;
+
+console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+// let str1 = "Hello"
+// let str2 = " Harish"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);//12
+// console.log(1 + "2");//12
+// console.log("1" + 2 + 2);//122 - The abstract operations takes argument input and optional argument(string or number).
+// console.log(1 + 2 + "2");
+
+// // console.log(3+4*5%3)//Wrong way
+// console.log((3 + 4) * 5 % 3);
+
+//Special Conversion
+
+// console.log(true);//true
+console.log(+true);//1
+// console.log(true+);//error
+
+console.log(+"");//0
+// console.log(""+);//0//error
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;// Wrong way
+
+let gameCounter = 100;
+gameCounter++;
+++gameCounter;
+
+console.log(gameCounter);
